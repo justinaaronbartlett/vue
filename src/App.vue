185481bb3c8dba3b77b1db8759c37f12b8c6ac14
justinaaronbartlett <144182158+justinaaronbartlett@users.noreply.github.com>
2024-04-22 01:01:30 +0000

    pushing to main

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive, onMounted } from 'vue';
 
-const newMsg = "new message!"
+const weatherData = reactive({
+  fahrenheit: null, 
+});
+
+onMounted(async () => {
+ 
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <h1>{{ newMsg }}</h1>
-  </main>
+  <div>
+    <p>Current Temperature: °F</p>
+  </div>
 </template>
 
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
